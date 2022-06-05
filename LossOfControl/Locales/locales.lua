079a21d9be42c31f15e46f38c4a07b8b80a67d90
NoGoodLife <100277966+nogoodlife@users.noreply.github.com>
2022-06-05 21:27:09 +0300

    Add files via upload

--- LossOfControl/Locales/locales.lua
+++ LossOfControl/Locales/locales.lua
@@ -1,4 +1,44 @@
-﻿if GetLocale() == "ruRU" then
+﻿    LOSS_OF_CONTROL_SECONDS = "seconds";
+
+    LOC_OPTION_ALERT = "Only Alert";
+    LOC_OPTION_FULL = "Show Full Duration";
+    LOC_OPTION_OFF = "Off";
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "Shows an alert in the middle of the screen when you lose control of your character.";
+    OPTION_LOSS_OF_CONTROL_DISARM = "When you have been disarmed. Abilities that require a weapon will be disabled.";
+    OPTION_LOSS_OF_CONTROL_FULL = "When you lose full control of your character from Banish, Charm, Stun, Disorient, Polymorph, Fear, Horror, Incapacitate, Sap, Sleep, Cyclone, and Shackle Undead.";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "When you have been interrupted. This also shows when you have been locked out of a specific spell school.";
+    OPTION_LOSS_OF_CONTROL_ROOT = "When you have been rooted. You cannot move but can still cast spells and do melee attacks.";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "When you have been silenced or pacified. You cannot cast some of your spells.";
+
+    LOC_TYPE_DISARM = "Disarm";
+    LOC_TYPE_FULL = "Full";
+    LOC_TYPE_INTERRUPT = "Interrupt";
+    LOC_TYPE_ROOT = "Root";
+    LOC_TYPE_SILENCE = "Silence";
+
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "Banished";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "Charmed";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "Confused";
+    LOSS_OF_CONTROL_DISPLAY_CYCLONE = "Cycloned";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "Dazed";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "Disarmed";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "Disoriented";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "Feared";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "Frozen";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "Horrified";
+    LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "Incapacitated";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s Locked";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "Pacified";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "Polymorphed";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "Possessed";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "Rooted";
+    LOSS_OF_CONTROL_DISPLAY_SAP = "Sapped";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "Shackled";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "Silenced";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "Asleep";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "Stunned";
+	
+if GetLocale() == "ruRU" then
     LOSS_OF_CONTROL_SECONDS = "секунд";
 
     LOC_OPTION_ALERT = "Только предупреждение";
@@ -39,44 +79,254 @@
     LOSS_OF_CONTROL_DISPLAY_SILENCE = "Немота";
     LOSS_OF_CONTROL_DISPLAY_SLEEP = "Сон";
     LOSS_OF_CONTROL_DISPLAY_STUN = "Оглушение";
-else
-    LOSS_OF_CONTROL_SECONDS = "seconds";
+	
+elseif GetLocale() == "deDE" then
+    LOSS_OF_CONTROL_SECONDS = "Sekunden";
 
-    LOC_OPTION_ALERT = "Only Alert";
-    LOC_OPTION_FULL = "Show Full Duration";
-    LOC_OPTION_OFF = "Off";
-    OPTION_TOOLTIP_LOSS_OF_CONTROL = "Shows an alert in the middle of the screen when you lose control of your character.";
-    OPTION_LOSS_OF_CONTROL_DISARM = "When you have been disarmed. Abilities that require a weapon will be disabled.";
-    OPTION_LOSS_OF_CONTROL_FULL = "When you lose full control of your character from Banish, Charm, Stun, Disorient, Polymorph, Fear, Horror, Incapacitate, Sap, Sleep, Cyclone, and Shackle Undead.";
-    OPTION_LOSS_OF_CONTROL_INTERRUPT = "When you have been interrupted. This also shows when you have been locked out of a specific spell school.";
-    OPTION_LOSS_OF_CONTROL_ROOT = "When you have been rooted. You cannot move but can still cast spells and do melee attacks.";
-    OPTION_LOSS_OF_CONTROL_SILENCE = "When you have been silenced or pacified. You cannot cast some of your spells.";
-
-    LOC_TYPE_DISARM = "Disarm";
-    LOC_TYPE_FULL = "Full";
-    LOC_TYPE_INTERRUPT = "Interrupt";
+    LOC_OPTION_ALERT = "Nur Warnung";
+    LOC_OPTION_FULL = "Vollständige Dauer anzeigen";
+    LOC_OPTION_OFF = "Aus";
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "Zeigt eine Warnung in der Mitte des Bildschirms an, wenn Sie die Kontrolle über Ihren Charakter verlieren.";
+    OPTION_LOSS_OF_CONTROL_DISARM = "Wenn Sie entwaffnet wurden. Fähigkeiten, die eine Waffe erfordern, werden deaktiviert.";
+    OPTION_LOSS_OF_CONTROL_FULL = "Wenn Sie durch Banish, Charm, Stun, Desorient, Polymorph, Fear, Horror, Incapacitate, Sap, Sleep, Cyclone und Fessel Untote die volle Kontrolle über Ihren Charakter verlieren.";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "Wenn Sie unterbrochen wurden. Dies zeigt auch, wenn Sie von einer bestimmten Zauberschule ausgeschlossen wurden.";
+    OPTION_LOSS_OF_CONTROL_ROOT = "Wenn Sie verwurzelt sind. Sie können sich nicht bewegen, aber immer noch Zauber wirken und Nahkampfangriffe ausführen.";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "Wenn Sie zum Schweigen gebracht oder beruhigt wurden. Einige Ihrer Zauber können Sie nicht wirken.";
+	
+	LOC_TYPE_DISARM = "Entwaffnen";
+    LOC_TYPE_FULL = "Voll";
+    LOC_TYPE_INTERRUPT = "Unterbrechen";
     LOC_TYPE_ROOT = "Root";
+    LOC_TYPE_SILENCE = "Stille";
+
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "Verbannt";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "Bezaubert";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "Verwirrt";
+	LOSS_OF_CONTROL_DISPLAY_CYCLONE = "Zykloniert";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "Benommen";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "Unscharf";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "Desorientiert";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "Gefürchtet";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "Eingefroren";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "Entsetzt";
+	LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "Gewehrt";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s gesperrt";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "Befriedet";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "Verwandelt";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "Besessen";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "Verwurzelt";
+	LOSS_OF_CONTROL_DISPLAY_SAP = "Sapped";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "Gefesselt";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "Stumm";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "Schlafend";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "Betäubt";
+	
+elseif GetLocale() == "esES" or GetLocale() == "esMX" then
+    LOSS_OF_CONTROL_SECONDS = "segundos";
+
+    LOC_OPTION_ALERT = "Solo Alerta";
+    LOC_OPTION_FULL = "Mostrar duración completa";
+    LOC_OPTION_OFF = "Desactivado";
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "Muestra una alerta en el medio de la pantalla cuando pierdes el control de tu personaje.";
+    OPTION_LOSS_OF_CONTROL_DISARM = "Cuando hayas sido desarmado. Las habilidades que requieran un arma serán deshabilitadas.";
+    OPTION_LOSS_OF_CONTROL_FULL = "Cuando pierdes el control total de tu personaje de Desterrar, Embelesar, Aturdir, Desorientar, Polimorfar, Miedo, Horror, Incapacitar, Savia, Sueño, Ciclón y Grillete no-muerto.";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "Cuando ha sido interrumpido. Esto también muestra cuando has sido excluido de una escuela de hechizos específica.";
+    OPTION_LOSS_OF_CONTROL_ROOT = "Cuando te hayan inmovilizado. No puedes moverte, pero aún puedes lanzar hechizos y realizar ataques cuerpo a cuerpo.";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "Cuando has sido silenciado o pacificado. No puedes lanzar algunos de tus hechizos.";
+	
+	LOC_TYPE_DESARM = "Desarmar";
+    LOC_TYPE_FULL = "Completo";
+    LOC_TYPE_INTERRUPT = "Interrumpir";
+    LOC_TYPE_ROOT = "Raíz";
+    LOC_TYPE_SILENCE = "Silencio";
+
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "Desterrado";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "Encantado";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "Confundido";
+	LOSS_OF_CONTROL_DISPLAY_CYCLONE = "Ciclado";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "Aturdido";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "Desarmado";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "Desorientado";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "Temeroso";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "Congelado";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "Horrorizado";
+	LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "Incapacitado";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s bloqueado";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "Tranquilizado";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "Polimórfico";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "Poseído";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "Enraizado";
+	LOSS_OF_CONTROL_DISPLAY_SAP = "Saqueado";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "Encadenado";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "Silenciado";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "Dormido";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "Aturdido";
+	
+elseif GetLocale() == "frFR" then
+    LOSS_OF_CONTROL_SECONDS = "secondes";
+
+    LOC_OPTION_ALERT = "Seulement Alerte";
+    LOC_OPTION_FULL = "Afficher la durée complète";
+    LOC_OPTION_OFF = "Désactivé";
+    
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "Affiche une alerte au milieu de lécran lorsque vous perdez le contrôle de votre personnage.";
+    OPTION_LOSS_OF_CONTROL_DISARM = "Lorsque vous avez été désarmé. Les capacités qui nécessitent une arme seront désactivées.";
+    OPTION_LOSS_OF_CONTROL_FULL = "Lorsque vous perdez le contrôle total de votre personnage avec Bannissement, Charme, Étourdissement, Désorientation, Métamorphose, Peur, Horreur, Incapacité, Sève, Sommeil, Cyclone et Entrave des morts-vivants.";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "Lorsque vous avez été interrompu. Cela montre également quand vous avez été exclu dune école de sorts spécifique.";
+    OPTION_LOSS_OF_CONTROL_ROOT = "Lorsque vous avez été enraciné. Vous ne pouvez pas vous déplacer mais pouvez toujours lancer des sorts et faire des attaques de mêlée.";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "Lorsque vous avez été réduit au silence ou pacifié. Vous ne pouvez pas lancer certains de vos sorts.";
+	
+	LOC_TYPE_DISARM = "Désarmer";
+    LOC_TYPE_FULL = "Complet";
+    LOC_TYPE_INTERRUPT = "Interrompre";
+    LOC_TYPE_ROOT = "Racine";
     LOC_TYPE_SILENCE = "Silence";
 
-    LOSS_OF_CONTROL_DISPLAY_BANISH = "Banished";
-    LOSS_OF_CONTROL_DISPLAY_CHARM = "Charmed";
-    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "Confused";
-    LOSS_OF_CONTROL_DISPLAY_CYCLONE = "Cycloned";
-    LOSS_OF_CONTROL_DISPLAY_DAZE = "Dazed";
-    LOSS_OF_CONTROL_DISPLAY_DISARM = "Disarmed";
-    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "Disoriented";
-    LOSS_OF_CONTROL_DISPLAY_FEAR = "Feared";
-    LOSS_OF_CONTROL_DISPLAY_FREEZE = "Frozen";
-    LOSS_OF_CONTROL_DISPLAY_HORROR = "Horrified";
-    LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "Incapacitated";
-    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s Locked";
-    LOSS_OF_CONTROL_DISPLAY_PACIFY = "Pacified";
-    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "Polymorphed";
-    LOSS_OF_CONTROL_DISPLAY_POSSESS = "Possessed";
-    LOSS_OF_CONTROL_DISPLAY_ROOT = "Rooted";
-    LOSS_OF_CONTROL_DISPLAY_SAP = "Sapped";
-    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "Shackled";
-    LOSS_OF_CONTROL_DISPLAY_SILENCE = "Silenced";
-    LOSS_OF_CONTROL_DISPLAY_SLEEP = "Asleep";
-    LOSS_OF_CONTROL_DISPLAY_STUN = "Stunned";
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "Banni";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "Charmé";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "Confus";
+	LOSS_OF_CONTROL_DISPLAY_CYCLONE = "Cyclonné";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "Étourdi";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "Désarmé";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "Désorienté";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "Craint";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "Gelé";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "Horrifié";
+	LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "Incapable";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s verrouillé";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "Pacifié";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "Polymorphé";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "Possédé";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "Enraciné";
+	LOSS_OF_CONTROL_DISPLAY_SAP = "Sapé";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "Enchaîné";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "Silencieux";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "En sommeil";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "Abasourdi";
+	
+elseif GetLocale() == "koKR" then
+    LOSS_OF_CONTROL_SECONDS = "초";
+
+    LOC_OPTION_ALERT = "알림만";
+    LOC_OPTION_FULL = "전체 기간 표시";
+    LOC_OPTION_OFF = "꺼짐";
+    
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "캐릭터에 대한 통제력을 잃으면 화면 중앙에 경고를 표시합니다.";
+    OPTION_LOSS_OF_CONTROL_DISARM = "무장 해제되었을 때. 무기가 필요한 능력은 비활성화됩니다.";
+    OPTION_LOSS_OF_CONTROL_FULL = "배니시, 매력, 기절, 방향 감각 상실, 변이, 공포, 공포, 무력화, 수액, 수면, 사이클론 및 언데드 족쇄에서 캐릭터의 모든 제어권을 잃으면";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "방해를 당했을 때. 특정 주문 학교에 접속할 수 없는 경우에도 표시됩니다.";
+    OPTION_LOSS_OF_CONTROL_ROOT = "접속 불가 상태일 때. 이동할 수는 없지만 주문을 시전하고 근접 공격을 할 수 있습니다.";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "침묵 또는 진정되었을 때. 일부 주문을 시전할 수 없습니다.";
+	
+	LOC_TYPE_DISARM = "무장 해제";
+    LOC_TYPE_FULL = "전체";
+    LOC_TYPE_INTERRUPT = "인터럽트";
+    LOC_TYPE_ROOT = "루트";
+    LOC_TYPE_SILENCE = "침묵";
+
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "추방됨";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "매력";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "혼란스러움";
+	LOSS_OF_CONTROL_DISPLAY_CYCLONE = "사이클론";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "멍해짐";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "무장 해제";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "방향 감각 상실";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "두려움";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "동결";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "무서움";
+	LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "무능력 상태";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s 잠김";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "진정함";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "변형됨";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "보유됨";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "루팅됨";
+	LOSS_OF_CONTROL_DISPLAY_SAP = "Sapped";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "속박";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "무음";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "수면";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "기절";
+	
+elseif GetLocale() == "enCN" or GetLocale() == "zhCN" then
+    LOSS_OF_CONTROL_SECONDS = "秒";
+
+    LOC_OPTION_ALERT = "只有警报";
+    LOC_OPTION_FULL = "显示完整持续时间";
+    LOC_OPTION_OFF = "关闭";
+    
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "当你失去对角色的控制时，在屏幕中间显示警告。";
+    OPTION_LOSS_OF_CONTROL_DISARM = "当你被缴械时。 需要武器的技能将被禁用。";
+    OPTION_LOSS_OF_CONTROL_FULL = "当你从放逐、魅惑、眩晕、迷失方向、变形、恐惧、恐惧、丧失能力、闷闷不乐、睡眠、飓风和束缚亡灵中失去对角色的完全控制时。";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "当你被打断时。 这也会显示您何时被锁定在特定的法术学校之外";
+    OPTION_LOSS_OF_CONTROL_ROOT = "当你被定身后。你不能移动但仍然可以施法和进行近战攻击。";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "当你被沉默或安抚时。你不能施放你的一些法术。";
+	
+	LOC_TYPE_DISARM = "解除武装";
+    LOC_TYPE_FULL = "满";
+    LOC_TYPE_INTERRUPT = "中断";
+    LOC_TYPE_ROOT = "根";
+    LOC_TYPE_SILENCE = "沉默";
+
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "被放逐";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "被迷住了";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "困惑";
+	LOSS_OF_CONTROL_DISPLAY_CYCLONE = "气旋";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "发呆";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "解除武装";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "迷失方向";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "害怕";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "冻结";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "吓坏了";
+	LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "丧失行动能力";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s 锁定";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "平息";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "变形";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "附身";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "植根";
+	LOSS_OF_CONTROL_DISPLAY_SAP = "被削弱";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "镣铐";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "沉默";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "睡着了";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "惊呆了";
+	
+elseif GetLocale() == "zhTW" then
+	LOSS_OF_CONTROL_SECONDS = "秒";
+
+    LOC_OPTION_ALERT = "只有警報";
+    LOC_OPTION_FULL = "顯示完整持續時間";
+    LOC_OPTION_OFF = "關閉";
+    
+    OPTION_TOOLTIP_LOSS_OF_CONTROL = "當你失去對角色的控制時，在屏幕中間顯示警告。";
+    OPTION_LOSS_OF_CONTROL_DISARM = "當你被繳械時。 需要武器的技能將被禁用。";
+    OPTION_LOSS_OF_CONTROL_FULL = "當你從放逐、魅惑、眩暈、迷失方向、變形、恐懼、恐懼、喪失能力、悶悶不樂、睡眠、颶風和束縛亡靈中失去對角色的完全控制時。";
+    OPTION_LOSS_OF_CONTROL_INTERRUPT = "當你被打斷時。 這也會顯示您何時被鎖定在特定的法術學校之外。";
+    OPTION_LOSS_OF_CONTROL_ROOT = "當你被定身後。你不能移動但仍然可以施法和進行近戰攻擊。";
+    OPTION_LOSS_OF_CONTROL_SILENCE = "當你被沉默或安撫時。你不能施放你的一些法術。";
+	
+	LOC_TYPE_DISARM = "解除武裝";
+    LOC_TYPE_FULL = "滿";
+    LOC_TYPE_INTERRUPT = "中斷";
+    LOC_TYPE_ROOT = "根";
+    LOC_TYPE_SILENCE = "沉默";
+
+    LOSS_OF_CONTROL_DISPLAY_BANISH = "被放逐";
+    LOSS_OF_CONTROL_DISPLAY_CHARM = "被迷住了";
+    LOSS_OF_CONTROL_DISPLAY_CONFUSE = "困惑";
+	LOSS_OF_CONTROL_DISPLAY_CYCLONE = "氣旋";
+    LOSS_OF_CONTROL_DISPLAY_DAZE = "發呆";
+    LOSS_OF_CONTROL_DISPLAY_DISARM = "解除武裝";
+    LOSS_OF_CONTROL_DISPLAY_DISORIENT = "迷失方向";
+    LOSS_OF_CONTROL_DISPLAY_FEAR = "害怕";
+    LOSS_OF_CONTROL_DISPLAY_FREEZE = "凍結";
+    LOSS_OF_CONTROL_DISPLAY_HORROR = "嚇壞了";
+	LOSS_OF_CONTROL_DISPLAY_INCAPACITATE = "喪失行動能力";
+    LOSS_OF_CONTROL_DISPLAY_INTERRUPT_SCHOOL = "%s 鎖定";
+    LOSS_OF_CONTROL_DISPLAY_PACIFY = "平息";
+    LOSS_OF_CONTROL_DISPLAY_POLYMORPH = "變形";
+    LOSS_OF_CONTROL_DISPLAY_POSSESS = "附身";
+    LOSS_OF_CONTROL_DISPLAY_ROOT = "植根";
+	LOSS_OF_CONTROL_DISPLAY_SAP = "被削弱";
+    LOSS_OF_CONTROL_DISPLAY_SHACKLE_UNDEAD = "鐐銬";
+    LOSS_OF_CONTROL_DISPLAY_SILENCE = "沉默";
+    LOSS_OF_CONTROL_DISPLAY_SLEEP = "睡著了";
+    LOSS_OF_CONTROL_DISPLAY_STUN = "驚呆了";
 end
