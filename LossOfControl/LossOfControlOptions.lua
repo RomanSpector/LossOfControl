319c43339585c4221148000d325c633ab6db7672
romanspector <ismailroma@ya.ru>
2023-12-16 14:00:17 +0300

    fix discord link

--- LossOfControl/LossOfControlOptions.lua
+++ LossOfControl/LossOfControlOptions.lua
@@ -160,7 +160,7 @@ function LossOfControl:OnInitialize()
 
     if ( not ROMANSPECTOR_DISCORD ) then
         ROMANSPECTOR_DISCORD = true;
-        DEFAULT_CHAT_FRAME:AddMessage("|cffbaf5aeCompactRaidFrame|r: See more |cff44d3e3https://discord.gg/4GTrkkaV9U|r");
+        DEFAULT_CHAT_FRAME:AddMessage("|cffbaf5aeLossOfControl|r: See more |cff44d3e3https://discord.gg/4GTrkkaV9U|r");
     end
 end
 
